546658b7ccb3d26620e66edfe25782f3248807c4
Loa Soumahoro <loagnadjou@gmail.com>
2024-07-08 05:12:39 +0200

    work in about me and button component

---
+++ src/components/button/index.jsx
@@ -0,0 +1,12 @@
+import './button.css';
+
+const Button = () => {
+  return (
+    <div>
+      <a href='#projects' className='button'>
+        View My Work
+      </a>
+    </div>
+  );
+};
+export default Button;
