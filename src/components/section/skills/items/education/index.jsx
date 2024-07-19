3732deb24d1ca359ab52bef71332d6023c1256f1
Loa Soumahoro <loagnadjou@gmail.com>
2024-07-19 17:41:24 +0200

    style the education section

--- src/components/section/skills/items/education/index.jsx
+++ src/components/section/skills/items/education/index.jsx
@@ -13,8 +13,10 @@ const Education = () => {
               <p className='education-container_items-date'>{item1.date}</p>
               <div className='education-container_items-infos'>
                 <h5>{item1.title}</h5>
-                <p>{item1.location}</p>
-                <p className='education-container_items'>
+                <p className='education-container_items-location' id='location'>
+                  {item1.location}
+                </p>
+                <p className='education-container_items-description'>
                   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo officiis consequatur hic culpa
                   corporis! Officia modi cumque dolor sapiente! nam.
                 </p>
@@ -24,15 +26,15 @@ const Education = () => {
         </div>
       </div>
       <div className='education-container'>
-        <h3>Education</h3>
+        <h3>Experience</h3>
         <div className='education-container_items'>
           {experience.map((item2, index) => (
             <div key={index}>
               <p className='education-container_items-date'>{item2.date}</p>
-              <div className='education-container_items-infos'>
+              <div className='education-container_items-infos' id='location'>
                 <h5>{item2.title}</h5>
-                <p>{item2.location}</p>
-                <p className='education-container_items'>{item2.description}</p>
+                <p className='education-container_items-location'>{item2.location}</p>
+                <p className='education-container_items-description'>{item2.description}</p>
               </div>
             </div>
           ))}
